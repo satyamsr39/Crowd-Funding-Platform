00f26a599cc30cecd7637cf9e9bebcf63f631d47
satyam <sars@gmail.com>
2025-08-26 13:43:12 +0530

    Issue fixed

--- frontend2/src/pages/NGOs.jsx
+++ frontend2/src/pages/NGOs.jsx
@@ -7,13 +7,18 @@ export default function NGOs(){
     fetch((import.meta.env.REACT_APP_BACKEND_URL || 'http://localhost:5000') + '/api/ngos')
       .then(r=>r.json()).then(setNgos)
   },[])
-  console.log(ngos)
+  const backendUrl = import.meta.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
   return (
     <div className="py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {ngos.map(n => (
         <div key={n._id} className="bg-white rounded-lg shadow hover:shadow-lg transition p-4">
           <div className="h-40 w-full bg-gray-100 rounded overflow-hidden mb-4">
-            <img src={`http://localhost:5000/uploads/${n.image}` || 'https://ashaf.org/wp-content/uploads/2020/12/PRYS1937.jpg'} alt={n.name} className="w-full h-full object-cover" />
+            {/* <img src={`http://localhost:5000/uploads/${n.image}` || 'https://ashaf.org/wp-content/uploads/2020/12/PRYS1937.jpg'} alt={n.name} className="w-full h-full object-cover" /> */}
+        <img 
+  src={n.image 
+    ? `${backendUrl}/uploads/${n.image}` 
+    : 'https://ashaf.org/wp-content/uploads/2020/12/PRYS1937.jpg'} 
+/>
           </div>
           <h3 className="text-lg font-semibold ">{n.name}</h3>
           <p className="text-sm text-gray-600 my-2">{n.description}</p>
